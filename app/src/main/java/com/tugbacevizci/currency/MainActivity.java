ee9961587e567af74fdfcf6e58ca5cbfab5504f8
Burak <burakiren@hotmail.com.tr>
2015-12-17 09:44:44 +0200

    #Düzenleme

--- app/src/main/java/com/tugbacevizci/currency/MainActivity.java
+++ app/src/main/java/com/tugbacevizci/currency/MainActivity.java
@@ -2,30 +2,34 @@ package com.tugbacevizci.currency;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import android.app.Activity;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    private TextView textView;
-    private EditText editText;
+    private TextView dovizTuru_tv, alis_tv, satis_tv;
+    private EditText alis_dolar_et, alis_euro_et, alis_gold_et, satis_dolar_et, satis_euro_et, satis_gold_et;
+    private Button refresh_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.dovizTuru_textView);
-        textView = (TextView)findViewById(R.id.alis_textView);
-        textView = (TextView)findViewById(R.id.satis_textView);
+        dovizTuru_tv = (TextView) findViewById(R.id.dovizTuru_textView);
+        alis_tv = (TextView) findViewById(R.id.alis_textView);
+        satis_tv = (TextView) findViewById(R.id.satis_textView);
 
-        editText = (EditText)findViewById(R.id.alis_dolar_editText);
-        editText = (EditText)findViewById(R.id.alis_euro_editText);
-        editText = (EditText)findViewById(R.id.alis_gold_editText);
+        alis_dolar_et = (EditText) findViewById(R.id.alis_dolar_editText);
+        alis_euro_et = (EditText) findViewById(R.id.alis_euro_editText);
+        alis_gold_et = (EditText) findViewById(R.id.alis_gold_editText);
 
-        editText = (EditText)findViewById(R.id.satis_dolar_editText);
-        editText = (EditText)findViewById(R.id.satis_euro_editText);
-        editText = (EditText)findViewById(R.id.satis_gold_editText);
+        satis_dolar_et = (EditText) findViewById(R.id.satis_dolar_editText);
+        satis_euro_et = (EditText) findViewById(R.id.satis_euro_editText);
+        satis_gold_et = (EditText) findViewById(R.id.satis_gold_editText);
+
+        refresh_btn = (Button) findViewById(R.id.refresh_btn);
     }
 }
